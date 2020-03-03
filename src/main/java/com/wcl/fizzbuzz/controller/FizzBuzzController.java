3c0c95a65e356c075cba24870da748cee385217d
warnettm coding.warnettconsultingltd@gmial.com <code.warnettconsultingltd@gmail.com>
2020-03-03 16:15:18 +0000

    Added FizzBuzzResult, was previously an inner class.

--- src/main/java/com/wcl/fizzbuzz/controller/FizzBuzzController.java
+++ src/main/java/com/wcl/fizzbuzz/controller/FizzBuzzController.java
@@ -1,13 +1,16 @@
 package com.wcl.fizzbuzz.controller;
 
+import com.wcl.fizzbuzz.entity.FizzBuzzResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public final class FizzBuzzController {
     @RequestMapping(method= RequestMethod.GET, value="/fizzbuzz/v1")
-    public String generateFizzBuzz() {
-        return "FizzBuzz";
+    public Optional<FizzBuzzResult> generateFizzBuzz() {
+        return Optional.empty();
     }
 }
---
+++ src/main/java/com/wcl/fizzbuzz/entity/FizzBuzzResult.java
@@ -0,0 +1,12 @@
+package com.wcl.fizzbuzz.entity;
+
+import lombok.Data;
+
+@Data
+public class FizzBuzzResult {
+    String range;
+    Integer fizz;
+    Integer buzz;
+    Integer fizzBuzz;
+    Integer integer;
+}
