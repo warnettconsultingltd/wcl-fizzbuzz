3614a47615ed8d19415ff4f03f794816ec99b2bd
warnettm coding.warnettconsultingltd@gmail.com <code.warnettconsultingltd@gmail.com>
2020-04-28 15:57:53 +0100

    Removed unit test runner.

--- src/test/java/com/wcl/fizzbuzz/integration/fizzbuzz/FizzBuzzStepDefinitions.java
+++ src/test/java/com/wcl/fizzbuzz/integration/fizzbuzz/FizzBuzzStepDefinitions.java
@@ -5,7 +5,6 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -13,11 +12,9 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FizzBuzzStepDefinitions {
     private final String URL = "http://localhost:%d/fizzbuzz/v1?start=%d&end=%d";
